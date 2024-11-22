d10fd0b10fe194afe58aface7c81b6cc4789a480
Chris Watson <cawatson1993@gmail.com>
2024-11-21 21:03:10 -0700

    ♻️ refactor: simplify version tracking by removing redundant commit hash
    The commit removes duplicate version tracking (Commit was same as Revision) and
    improves PKGBUILD generation with more reliable version string manipulation

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -8,6 +8,3 @@ const Revision = "2a96c314e21add8076e7b0ee756c961f9cd46171"
 
 // Date is the build date of goshot
 const Date = "2024-11-22"
-
-// Commit is the git commit of goshot
-const Commit = "2a96c314e21add8076e7b0ee756c961f9cd46171"
