2a96c314e21add8076e7b0ee756c961f9cd46171
Chris Watson <cawatson1993@gmail.com>
2024-11-21 20:56:36 -0700

    ✨ feat: add version metadata including revision, date and commit hash

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,3 +2,12 @@ package version
 
 // Version is the current version of goshot
 const Version = "v0.4.4"
+
+// Revision is the current git revision of goshot
+const Revision = "HEAD"
+
+// Date is the build date of goshot
+const Date = "unknown"
+
+// Commit is the git commit of goshot
+const Commit = "unknown"
