659c7d546d83d242177b92106f81233801227ff0
Chris Watson <cawatson1993@gmail.com>
2024-11-22 11:57:51 -0700

    fix dirty goreleaser

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,5 @@
 package version
 
-const (
-	Version  = "v0.4.10"
-	Revision = "6244d4777d3d8184294e80985bc8cbfe1d9bd771"
-	Date     = "2024-11-22"
-)
+const Version = "v0.4.10"
+const Revision = "6244d4777d3d8184294e80985bc8cbfe1d9bd771"
+const Date = "2024-11-22"
