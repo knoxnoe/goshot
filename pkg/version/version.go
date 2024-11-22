7ad7f7b3f01059635edb7d59448b99330c99b115
Chris Watson <cawatson1993@gmail.com>
2024-11-22 10:38:24 -0700

    Prepare for v0.4.11 release

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,10 +1,8 @@
 package version
 
 // Version is the current version of goshot
-const Version = "v0.4.10"
-
-// Revision is the current git revision of goshot
-const Revision = "a83d3227d9d56e32dc09e5f5fe7f1171cd821afc"
-
-// Date is the build date of goshot
-const Date = "2024-11-22"
+const (
+	Version = "v0.4.11"
+	Revision = ""
+	Date = ""
+)
